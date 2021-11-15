a20e34ccb0dad08fd0a8d91f9395ca05557a83b4
Larry Pallozzi <raijin22@outlook.com>
2021-11-14 22:11:10 -0900

    function_tools

---
+++ mytoolbox/Function_tools.py
@@ -0,0 +1,11 @@
+def say_hello(word):
+    if word =='Hello':
+        return 'Hello!'
+    else:
+        return 'Bye'
+
+def long_word(word):
+    if word > 5:
+        return 'long'
+    else:
+        return 'short'
